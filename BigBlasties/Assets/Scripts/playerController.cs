6b36bc1f0b14896e2f6fb9618e6c1297944a82ea
Steven Demas <162354807+Sdprograms@users.noreply.github.com>
2024-11-03 15:24:08 -0500

    JumpAndSprint
    Jumping and sprinting for the player is now functional

--- BigBlasties/Assets/Scripts/playerController.cs
+++ BigBlasties/Assets/Scripts/playerController.cs
@@ -12,12 +12,14 @@ public class playerController : MonoBehaviour
     [SerializeField] int speed;
     [SerializeField] int sprintMult;
     [SerializeField] int jumpMax;
+    [SerializeField] int jumpCount;
     [SerializeField] int jumpSpeed;
     [SerializeField] int gravity;
 
     Vector3 moveDirection;
     Vector3 playerVel;
 
+    bool isSprinting;
 
     // Start is called before the first frame update
     void Start()
@@ -29,14 +31,50 @@ public class playerController : MonoBehaviour
     void Update()
     {
         movement();
+        sprint();
     }
 
     void movement()
     {
+        if ((characterController.isGrounded))
+        {
+            jumpCount = 0;
+            playerVel = Vector3.zero;
+        }
+
         moveDirection = transform.forward * Input.GetAxis("Vertical") //Establish move Direction
             + transform.right * Input.GetAxis("Horizontal");
 
         characterController.Move(moveDirection * speed * Time.deltaTime);
 
+        jump();
+
+        characterController.Move(playerVel * Time.deltaTime);
+
+        playerVel.y -= gravity * Time.deltaTime;
+
+    }
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpMax)
+        {
+            jumpCount++;
+            playerVel.y = jumpSpeed;
+        }
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMult;
+            isSprinting = true;
+        }
+        else if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMult;
+            isSprinting = false;
+        }
     }
 }

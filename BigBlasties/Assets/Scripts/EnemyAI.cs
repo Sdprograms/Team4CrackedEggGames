2b9762cb30256db7b6c46f005252cd9c49126159
KhaliDino <103606561+A-KhaliDino@users.noreply.github.com>
2024-11-03 18:14:59 -0500

    Added some placeholders, simple prefabs,

---
+++ BigBlasties/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour, CanDamage
+{
+    [SerializeField] Renderer model;
+
+    [SerializeField] int HP;
+    [SerializeField] int MaxHP;
+
+    Color orginalColor;
+   
+    void Start()
+    {
+        orginalColor = model.material.color;
+        HP = MaxHP;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        StartCoroutine(hitmarker());
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator hitmarker()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = orginalColor;
+    }
+}

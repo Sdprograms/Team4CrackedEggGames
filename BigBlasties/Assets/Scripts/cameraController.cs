cbb769c2939d3db4ce3c52f5b5a2da584ccaa089
Steven Demas <162354807+Sdprograms@users.noreply.github.com>
2024-11-03 14:49:32 -0500

    CameraController
    Basic Camera controller added. Looking with the mouse is now functional.

--- BigBlasties/Assets/Scripts/cameraController.cs
+++ BigBlasties/Assets/Scripts/cameraController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class cameraController : MonoBehaviour
 {
+
+    [SerializeField] int lookSensitivity;
+    [SerializeField] int lockVerticalmin, lockVerticalmax;
+    [SerializeField] bool invertY = false;
+
+    float rotX;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false; //Cursor settings on start
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseY = Input.GetAxis("Mouse Y") * lookSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * lookSensitivity * Time.deltaTime;
+
+        if (invertY) //If Mouse invert is selected
+        {
+            rotX += mouseY;
+        }
+        else
+        {
+            rotX -= mouseY;
+        }
+
+        rotX = Mathf.Clamp(rotX, lockVerticalmin, lockVerticalmax); //Camera clamp
+
+        transform.localRotation = Quaternion.Euler(rotX, 0, 0); //Rotate on x-axis
+
+        transform.parent.Rotate(Vector3.up * mouseX); //Rotate on Y-axis
     }
 }

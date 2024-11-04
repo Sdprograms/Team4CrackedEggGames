f1aea15a596ef0d1510843ff1fafede63804e82c
KhaliDino <103606561+A-KhaliDino@users.noreply.github.com>
2024-11-03 21:25:00 -0500

    deleted redundant CanDamage script, updated enemyAI

--- BigBlasties/Assets/Scripts/EnemyAI.cs
+++ BigBlasties/Assets/Scripts/EnemyAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour, CanDamage
+public class EnemyAI : MonoBehaviour, damageInterface
 {
     [SerializeField] Renderer model;
 

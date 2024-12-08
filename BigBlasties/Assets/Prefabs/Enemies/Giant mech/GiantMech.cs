53672dd356461bbf169d77bf21b1fe4b54326262
KhaliDino <103606561+A-KhaliDino@users.noreply.github.com>
2024-12-08 16:35:49 -0500

    Boss mech behaviours added (movement disabled until its made enjoyable)

--- BigBlasties/Assets/Prefabs/Enemies/Giant mech/GiantMech.cs
+++ BigBlasties/Assets/Prefabs/Enemies/Giant mech/GiantMech.cs
@@ -15,7 +15,11 @@ public class GiantMech : MonoBehaviour, damageInterface
     [SerializeField] NavMeshAgent agent;
     [SerializeField] GameObject bullet;
     [SerializeField] GameObject rocket;
+    [SerializeField] GameObject grenademotor;
     [SerializeField] float attackRate;
+    [SerializeField] float MachineGunattackRate;
+    [SerializeField] float MotorattackRate;
+    [SerializeField] float BlastattackRate;
     [SerializeField] int turnSpeed;
     [SerializeField] int viewAngle;
     [SerializeField] GameObject sheild;
@@ -23,16 +27,21 @@ public class GiantMech : MonoBehaviour, damageInterface
     [SerializeField] Transform ArenaCenter;
     [SerializeField] float spawnRadius = 50f;
     [SerializeField] ItemDrop dropScript;
-    public float mVelocity;
 
     private List<GameObject> activeBatteryCells = new List<GameObject>();
     [SerializeField] private Animator animator;
     bool isAttacking;
     bool batteryspawned;
     Vector3 playerPos;
-    Vector3 batteryPos;
     private EnemyDetection detector; // this is necessary in order for each enemy to have their own bubble,
                                      // otherwise without this all enemies will respond to one enemy bubble and not their own -XB
+
+    private enum AttackType
+    {
+        Motor,
+        Blast,
+        MachineGun
+    }
 
     // on start set HP to max HP, saving hp and Max HP seperately for possible 'next level' functionality.
     void Start()
@@ -44,17 +53,6 @@ public class GiantMech : MonoBehaviour, damageInterface
 
     void Update()
     {
-        if (agent.velocity.magnitude > 0.1f)
-        {
-            animator.SetBool("IsMove", true);
-        }
-        else
-        {
-            animator.SetBool("IsMove", false);
-        }
-
-        mVelocity = agent.velocity.magnitude;
-
         if (detector.playerInRange)
         {
             playerPos = GameManager.mInstance.mPlayer.transform.position - sightPos.position;
@@ -138,9 +136,7 @@ public class GiantMech : MonoBehaviour, damageInterface
         {
             if (dropScript != null)
                 dropScript.Drop();
-            animator.Play("Hit");
-            agent.speed = 0;
-            Destroy(gameObject, 5f);
+            Destroy(gameObject);
             GameManager.mInstance.mEnemyDamageHitmarker.SetActive(false);
 
         }
@@ -156,14 +152,51 @@ public class GiantMech : MonoBehaviour, damageInterface
 
     IEnumerator attack()
     {
-        animator.SetBool("IsMove", false);
-        animator.SetBool("IsAttack", true);
+        float attackDuration = 5f;
+        float startTime = Time.time;
+        float maxDeviationAngle = 15f;
         isAttacking = true;
-        Vector3 directionToPlayer = (GameManager.mInstance.mPlayer.transform.position - attackPos.position).normalized;
-        Instantiate(bullet, attackPos.position, Quaternion.LookRotation(directionToPlayer));
+        AttackType attackType = (AttackType)Random.Range(0, 3);
+        while (Time.time < startTime + attackDuration)
+        {
+            Vector3 directionToPlayer = (GameManager.mInstance.mPlayer.transform.position - attackPos.position).normalized;
+            float randomDeviation = Random.Range(-maxDeviationAngle, maxDeviationAngle);
+            Quaternion deviationRotation = Quaternion.Euler(0, randomDeviation, 0);
+            Vector3 deviatedDirection = deviationRotation * directionToPlayer;
+            switch (attackType)
+            {
+                case AttackType.Motor:
+                    int projectileCount = 10;
+                    for (int i = 0; i < projectileCount; i++)
+                    {
+                        Vector3 launchDirection = new Vector3(deviatedDirection.x, 1f, deviatedDirection.z).normalized;
+                        float launchHeight = 2.0f;
+                        float launchForce = Random.Range(1f, 50f);
+                        GameObject projectile = Instantiate(grenademotor, attackPos.position, Quaternion.LookRotation(launchDirection));
+                        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+                        if (rb != null)
+                        {
+                            rb.AddForce(launchDirection * launchForce + Vector3.up * launchHeight, ForceMode.Impulse);
+                        }
+
+                        yield return new WaitForSeconds(0.1f);
+                    }
+                    yield return new WaitForSeconds(MotorattackRate);
+                    break;
+
+                case AttackType.Blast:
+                    Instantiate(rocket, attackPos2.position, Quaternion.LookRotation(deviatedDirection));
+                    yield return new WaitForSeconds(BlastattackRate);
+                    break;
+
+                case AttackType.MachineGun:
+                    Instantiate(bullet, attackPos.position, Quaternion.LookRotation(deviatedDirection));
+                    yield return new WaitForSeconds(MachineGunattackRate);
+                    break;
+            }
+        }
         yield return new WaitForSeconds(attackRate);
         isAttacking = false;
-        animator.SetBool("IsAttack", false);
     }
 
     void facetarget()
